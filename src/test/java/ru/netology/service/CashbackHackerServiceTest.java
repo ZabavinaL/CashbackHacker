864e63816e06e06a865d43e7996b0963e819f0c8
Lyubov Zabavina <zabavinal@yandex.ru>
2021-02-27 00:03:13 +0300

    build.gradle changed with junit4

--- src/test/java/ru/netology/service/CashbackHackerServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackerServiceTest.java
@@ -1,8 +1,7 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
-
-import static org.testng.Assert.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class CashbackHackerServiceTest {
 
