fa7b94f97deda7666d71fff318f9a58d118f0f59
Lyubov Zabavina <zabavinal@yandex.ru>
2021-02-27 09:35:39 +0300

    import edited

--- src/test/java/ru/netology/service/CashbackHackerServiceApiTest.java
+++ src/test/java/ru/netology/service/CashbackHackerServiceApiTest.java
@@ -1,6 +1,6 @@
 package ru.netology.service;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CashbackHackerServiceApiTest {
